80fa5d74d96e5a158e9645c4eb27bfa1eaf0b697
Drakoxw <dragon12xw@gmail.com>
2026-05-24 19:28:44 -0500

    fix: make /files/:id/serve and /files/:id/thumbnail public (no auth)

--- internal/modules/file/module.go
+++ internal/modules/file/module.go
@@ -15,6 +15,10 @@ func NewModule(hdl *Handler) *Module {
 func (m *Module) Name() string { return "file" }
 
 func (m *Module) Register(rg *gin.RouterGroup, authMw, _ gin.HandlerFunc) {
+	// Public routes (no auth)
+	rg.GET("/files/:id/serve", m.hdl.ServeFile)
+	rg.GET("/files/:id/thumbnail", m.hdl.ServeThumbnail)
+
 	g := rg.Group("/files")
 	g.Use(authMw)
 	{
@@ -22,8 +26,6 @@ func (m *Module) Register(rg *gin.RouterGroup, authMw, _ gin.HandlerFunc) {
 		g.GET("/trash", m.hdl.ListTrash)
 		g.GET("", m.hdl.ListFiles)
 		g.GET("/:id", m.hdl.GetFile)
-		g.GET("/:id/serve", m.hdl.ServeFile)
-		g.GET("/:id/thumbnail", m.hdl.ServeThumbnail)
 		g.DELETE("/:id", m.hdl.SoftDelete)
 		g.POST("/:id/restore", m.hdl.Restore)
 		g.POST("/:id/recover-temp", m.hdl.RecoverTemp)
