2f5f1430acf79ca9ea7f94efa33c465c73f54909
Drakoxw <dragon12xw@gmail.com>
2026-05-26 16:16:24 -0500

    fix: nano banana x1

--- internal/modules/studio/image/generators/gentypes.go
+++ internal/modules/studio/image/generators/gentypes.go
@@ -2,5 +2,5 @@ package generators
 
 // ValidResolutionsImage lists supported image resolutions.
 var ValidResolutionsImage = map[string]bool{
-	"2K": true, "1080p": true, "720p": true,
+	"480p": true, "720p": true, "1080p": true, "1K": true, "2K": true, "4K": true,
 }
